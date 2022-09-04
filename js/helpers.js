f78c5fa016d2dd0710dd5ec2e9df0aebb3623623
matias cardoso <mattiascardoso10@gmail.com>
2022-09-04 15:25:32 -0300

    agrege validaciones al mail y a la contraseña y tambien los requisitos de la contraseña en el html

--- js/helpers.js
+++ js/helpers.js
@@ -11,3 +11,29 @@ export function cantidadCaracteres(input){
     }
 }
 
+
+let validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+export function validarEmail(input){
+    if( validEmail.test(input.value) ){
+		input.className = "form-control is-valid";
+		return true;
+	}else{
+		input.className = "form-control is-invalid";
+		return false;
+	}
+} 
+
+
+let validContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+
+export function validarContraseña(input){
+    if( validContraseña.test(input.value)){
+        input.className = "form-control is-valid";
+		return true;
+    }else{
+		input.className = "form-control is-invalid";
+		return false;
+	}
+}
+
--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 
-import { cantidadCaracteres } from "./helpers.js";
+import { cantidadCaracteres, validarContraseña, validarEmail } from "./helpers.js";
 import {Usuario} from "./claseUsuario.js";
 
 let formulario = document.querySelector("#formularioEntero");
@@ -7,8 +7,11 @@ let nombreIngresado = document.querySelector('#nombreCompleto');
 let mailIngresado = document.querySelector('#mailIngresado');
 let contraseñaIngresada = document.querySelector('#contraseñaIngresada');
 
+
 formulario.addEventListener("submit", crearUsuario);
 nombreIngresado.addEventListener("blur", ()=>{cantidadCaracteres(nombreIngresado)});
+mailIngresado.addEventListener("blur", ()=> {validarEmail(mailIngresado)});
+contraseñaIngresada.addEventListener("blur", ()=>{validarContraseña(contraseñaIngresada)});
 
 
 function crearUsuario(e){
