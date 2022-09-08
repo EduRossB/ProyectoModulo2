34c5f94c2940305af88c95ebceb365d9b8cba361
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-09-08 16:44:24 -0300

    agregamos mas cards con javasccript

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
 const productos = [
   {
     id: uuidv4(),
-    name: "Notebook HP",
-    img: "https://images.start.com.ar/26N51LT-3.jpg",
-    descripcion: "La notebook más potente",
+    name: "IPhone 14 Pro",
+    img: "https://memeandchill.com/wp-content/uploads/2022/09/iPhone-14-Pro-and-Pro-Max-.png",
+    descripcion: "",
     new: true,
   },
   {
     id: uuidv4(),
-    name: "Samsung S20",
-    img: "https://images.samsung.com/mx/smartphones/galaxy-s20/models/images/galaxy-s20_models_360_kv_s.jpg",
-    descripcion: "El mejor celular de la gama alta de samsung",
-    new: false,
+    name: "Notebook ThinkPad E14 2da Gen",
+    img: "https://www.notebookcheck.org/fileadmin/Notebooks/Lenovo/ThinkPad_E14_Gen2_20RA001CGE/lenovo_thinkpad_e14_teaser.jpg",
+    descripcion: "",
+    new: true,
   },
+  {
+    id: uuidv4(),
+    name: "Celular Motorola G51",
+    img: "https://player8.org/wp-content/uploads/2022/03/moto-g51-890x606.jpg",
+    descripcion: "",
+    new: true,
+  }
 ];
 
 const sectionProducts = document.getElementById("sectionProducts");
@@ -23,7 +30,7 @@ const displayProducts = () => {
       `<div>
         <div class="card cardsNuevosProductos bg-secondary">
           <img src=${producto.img} class="card-img-top" alt=${producto.name}>
-          <div class="card-body ">
+          <div class="card-back">
             <h5 class="card-title text-light">${producto.name}
             ${producto.new ? '<span class="badge rounded-pill text-bg-warning ">Nuevo</span>' : ''}
             </h5>
