e142333c108e9b6e9296458296973b76bfee5587
flor.Palma <contr.lucascordoba98@gmail.com>
2022-09-13 19:41:53 -0300

    ultimos cambios carrito

--- js/stock.js
+++ js/stock.js
@@ -1,6 +1,6 @@
 let stockProductos = [
-    {id: 1, nombre: "Lenovo", tipo: "Notebook", cantidad: 1,desc: "descripcion producto", precio: 1200, img: './img/lenovo.jpg'},
-    {id: 2, nombre: "Acer", tipo: "Notebook", cantidad: 1,desc: "descripcion producto", precio: 1100, img: './img/acer.jpg'},
-    {id: 3, nombre: "Apple", tipo: "buzo", cantidad: 1,desc: "descripcion producto",  precio: 1200,  img: './img/apple.jpg'},
-    {id: 4, nombre: "hp", tipo: "buzo", cantidad: 1,desc: "descripcion producto",  precio: 1400, img: './img/hp1.jpg'},
+    {id: 1, nombre: "Lenovo", tipo: "Notebook", cantidad: 1,desc: "IdeaPad S145 Core i7 4gb 1tb 15.6", precio: 150000, img: './img/lenovo.jpg'},
+    {id: 2, nombre: "Acer", tipo: "Notebook", cantidad: 1,desc: "AN515-57-52NY intel core i5 16gb", precio: 189000, img: './img/acer.jpg'},
+    {id: 3, nombre: "Apple", tipo: "Notebook", cantidad: 1,desc: "Macbook Air M1 8gb ram 256gb Ssd",  precio: 269000,  img: './img/apple.jpg'},
+    {id: 4, nombre: "Hp", tipo: "Notebook", cantidad: 1,desc: "14-CF2535LA Intel core i5 8gb",  precio: 154000, img: './img/hp1.jpg'},
 ]
